b75fe3983aa2eb446c45ba7ff18963c181014b77
HX <kidztse@gmail.com>
2022-12-02 12:39:26 +0100

    DI from minification, one way is to put propreties in array, better solution would be

--- practiceAlongTheCourse/Course1/DependencyInjection/DIApp.js
+++ practiceAlongTheCourse/Course1/DependencyInjection/DIApp.js
@@ -1,17 +1,24 @@
-(function () {
-    "use strict";
+// (function () {
+//     "use strict";
 
-    var DIAppJS = angular.module("DIApp", []);
+//     var DIAppJS = angular.module("DIApp", []);
 
-    DIAppJS.controller("DIController", DIController);
+//     DIAppJS.controller("DIController", DIController);
 
-    function DIController($scope, $filter) {
-        $scope.name = "Huan";
+//     DIController.$inject = ['$scope','$filter'];
+//     function DIController($scope, $filter) {
+//         $scope.name = "Huan";
 
-        $scope.upper = function () {
-            var upCase = $filter("uppercase");
-            $scope.name = upCase($scope.name);
-        };
-    }
+//         $scope.upper = function () {
+//             var upCase = $filter("uppercase");
+//             $scope.name = upCase($scope.name);
+//         };
+//     }
 
-})();
+// })();
+
+//inline array solution:
+// angular.module("DIApp",[]).controller("DIController",["$scope","$filter",function e(n,o){n.name="Huan",n.upper=function(){var e=o("uppercase");n.name=e(n.name)}}]);
+
+// $inject property to the function object
+!function(){"use strict";function e(e,n){e.name="Huan",e.upper=function(){var o=n("uppercase");e.name=o(e.name)}}angular.module("DIApp",[]).controller("DIController",e),e.$inject=["$scope","$filter"]}();
