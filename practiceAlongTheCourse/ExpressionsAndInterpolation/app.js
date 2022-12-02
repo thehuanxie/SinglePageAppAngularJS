c899c39e2570913b0d2994489d96fa261edf7082
HX <kidztse@gmail.com>
2022-12-02 13:57:12 +0100

    Expression and Interpolation can be used to trigger the button

--- practiceAlongTheCourse/ExpressionsAndInterpolation/app.js
+++ practiceAlongTheCourse/ExpressionsAndInterpolation/app.js
@@ -8,8 +8,14 @@
 
     function MsgController($scope){
         $scope.name = "Huan";
+        $scope.stateOfBeing = "waiting";
+
         $scope.sayMessage = function(){
             return "Huan likes to paint :)"
+        };
+
+        $scope.callLatte = function(){
+            $scope.stateOfBeing = "called";
         };
     }
 
