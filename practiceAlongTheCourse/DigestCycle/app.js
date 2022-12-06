6b3b710604d2b70e72407563bd073e4ad7a15797
HX <kidztse@gmail.com>
2022-12-06 17:21:55 +0100

    set watchers manually

--- practiceAlongTheCourse/DigestCycle/app.js
+++ practiceAlongTheCourse/DigestCycle/app.js
@@ -1,16 +1,35 @@
 (function () {
-'use strict';
+  'use strict';
 
-angular.module('CounterApp', [])
-.controller('CounterController', CounterController);
+  angular.module('CounterApp', [])
+    .controller('CounterController', CounterController);
 
-CounterController.$inject = ['$scope'];
-function CounterController($scope) {
+  CounterController.$inject = ['$scope'];
+  function CounterController($scope) {
+    $scope.onceCounter = 0;
+    $scope.counter =0;
 
-  $scope.showNumberOfWatchers = function () {
+    $scope.showNumberOfWatchers = function () {
+      console.log('# of watchers', $scope.$$watchersCount);
+    };
 
-  };
+    $scope.countOnce = function () {
+      $scope.onceCounter = 1;
+    };
 
-}
+    $scope.upCounter = function (){
+        $scope.counter++;
+    };
+
+    $scope.$watch('onceCounter', function (newValue, oldValue) {
+      console.log("old value: ", oldValue);
+      console.log("new value:", newValue);
+    });
+
+    $scope.$watch('counter', function (newValue, oldValue) {
+      console.log("counter old value: ", oldValue);
+      console.log("counter new value:", newValue);
+    });
+  }
 
 })();
