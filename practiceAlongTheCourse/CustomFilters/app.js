c14cd6b1d18451e2d98c06a3d9deaf7fbd6a30e6
HX <kidztse@gmail.com>
2022-12-06 14:41:53 +0100

    customFilter practice

---
+++ practiceAlongTheCourse/CustomFilters/app.js
@@ -0,0 +1,40 @@
+(function(){
+    'use strict';
+
+    angular.module('MsgApp',[])
+    .controller('MsgController', MsgController)
+    .filter('loves',LovesFilterFactory);
+
+    MsgController.$inject = ['$scope','lovesFilter','$filter'];
+    //$inject array elements have respect the same order than controler arguments order
+    function MsgController($scope, lovesFilter, $filter){
+        $scope.name = "Huan";
+        $scope.stateOfBeing = "waiting";
+        $scope.LatteEat = .72;
+
+        $scope.sayMessage = function(){
+            var msg = "Huan likes to paint :)";
+            var output = $filter('uppercase')(msg);
+            return output;
+        };
+        
+        $scope.saylovesMessage = function(){
+            var msg = "Huan likes to paint :)";
+            var output = lovesFilter(msg);
+            return output;
+        };
+
+        $scope.callLatte = function(){
+            $scope.stateOfBeing = "called";
+        };
+    }
+
+    function LovesFilterFactory(){
+        return function (input){
+            input = input || "";
+            input = input.replace("likes", "loves");
+            return input;
+        };
+    };
+
+})();
