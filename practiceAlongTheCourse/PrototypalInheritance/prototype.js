aff7c4b8ee70f914b809369fcf510396461e2739
HX <kidztse@gmail.com>
2023-03-25 12:57:52 +0100

    update modification

--- practiceAlongTheCourse/PrototypalInheritance/prototype.js
+++ practiceAlongTheCourse/PrototypalInheritance/prototype.js
@@ -8,7 +8,7 @@
 //     console.log("walking!");
 //   }
 // };
-//
+
 // var child = Object.create(parent);
 // console.log("CHILD - child.value: ", child.value);
 // console.log("CHILD - child.obj.objValue: ", child.obj.objValue);
@@ -16,7 +16,7 @@
 // console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
 // console.log("parent: ", parent);
 // console.log("child: ", child);
-//
+
 // child.value = "childValue";
 // child.obj.objValue = "childObjValue";
 // console.log("*** CHANGED: child.value = 'childValue'");
@@ -27,23 +27,23 @@
 // console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
 // console.log("parent: ", parent);
 // console.log("child: ", child);
-//
+
 // console.log("child.obj === parent.obj ? ", child.obj === parent.obj);
-//
+
 // var grandChild = Object.create(child);
 // console.log("Grandchild: ", grandChild);
 // grandChild.walk();
 
 //** Function constructors
-// See my other course: HTML, CSS, and Javascript for Web Developers
-// Lecture #48
-// function Dog(name) {
-//   this.name = name;
-//   console.log("'this' is: ", this);
-// }
-//
-// var myDog = new Dog("Max");
-// console.log("myDog: ", myDog);
-//
-// // Not being used as a function constructor
-// Dog("Max2");
+//See my other course: HTML, CSS, and Javascript for Web Developers
+//Lecture #48
+function Dog(name) {
+  this.name = name;
+  console.log("'this' is: ", this);
+}
+
+var myDog = new Dog("Max");
+console.log("myDog: ", myDog);
+
+// Not being used as a function constructor
+Dog("Max2");
